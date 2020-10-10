0291950cee2e71cc64952968776919ff01510f48
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2020-10-10 06:13:51 -0400

    Refactored folder structure

--- app/react/containers/Window.tsx
+++ app/react/containers/Window.tsx
@@ -1,13 +1,13 @@
 /* eslint react/jsx-props-no-spreading: off */
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import routes from './constants/routes.json';
-import App from './containers/App';
-import HomePage from './containers/HomePage';
+import routes from '../constants/routes.json';
+import App from './App';
+import Home from '../pages/home/Home';
 
 // Lazily load routes and code split with webpack
 const LazyCounterPage = React.lazy(() =>
-  import(/* webpackChunkName: "CounterPage" */ './containers/CounterPage')
+  import(/* webpackChunkName: "CounterPage" */ '../pages/counter/Counter')
 );
 
 const CounterPage = (props: Record<string, any>) => (
@@ -31,7 +31,7 @@ export default function Routes() {
       </div>
       <Switch>
         <Route path={routes.COUNTER} component={CounterPage} />
-        <Route path={routes.HOME} component={HomePage} />
+        <Route path={routes.HOME} component={Home} />
       </Switch>
     </App>
   );
--- app/react/pages/home/Home.tsx
+++ app/react/pages/home/Home.tsx
@@ -1,27 +1,24 @@
 import BrowserView from 'react-electron-browser-view';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import routes from '../constants/routes.json';
+import routes from '../../constants/routes.json';
 import styles from './Home.css';
 
 export default function Home(): JSX.Element {
   return (
-    <div
-      style={{ display: 'flex', flexDirection: 'row-reverse', height: '100vh' }}
-    >
+    <div style={{ display: 'flex', flexDirection: 'row', height: '100vh' }}>
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
         <Link to={routes.COUNTER}>to Counter</Link>
       </div>
-      <div>
-        <BrowserView
-          src="https://notion.so"
-          style={{
-            height: '100%',
-            width: '100%',
-          }}
-        />
-      </div>
+
+      <BrowserView
+        src="https://notion.so"
+        style={{
+          height: '100%',
+          width: '100%',
+        }}
+      />
     </div>
   );
 }
